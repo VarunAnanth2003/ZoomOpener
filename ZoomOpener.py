fe308250e93eab9ec63ff88b171574bb60f87aba
Varun Ananth <varunananth1@gmail.com>
2021-05-04 10:18:50 -0700

    Fixed misalignment bug

--- ZoomOpener.py
+++ ZoomOpener.py
@@ -2,13 +2,13 @@ import webbrowser
 import json
 import time
 from threading import Timer
-currentPeriod = 0
 classDict = json.load(open('zoomdata.json'))
 currentInfo = time.localtime(time.time())
-allTimes = []
+allTimes = {}
 
 Timer.cancel
 print("\n" + "SCHEDULE:" + "\n" + "---------------")
+
 for index, info in enumerate(classDict['classData']):
     newTime = time.mktime(time.struct_time((currentInfo.tm_year, currentInfo.tm_mon, currentInfo.tm_mday, info["timeStartHour"], info["timeStartMinute"], 0, currentInfo.tm_wday, currentInfo.tm_yday, currentInfo.tm_isdst)))
     wday = currentInfo.tm_wday
@@ -16,23 +16,21 @@ for index, info in enumerate(classDict['classData']):
         newTime += 86400
     if(classDict["daysOnline"][time.localtime(newTime).tm_wday]):
         if(classDict['classActive'][index]):
-            allTimes.append(newTime-time.time())
             print(info["name"] + ((35-(len(info["name"]))) * " ") + " @ " + time.asctime(time.localtime(newTime)))
         else:
             print(info["name"] + ((35-(len(info["name"]))) * " ") + " @ " + "ASYNCHRONOUS")
+        allTimes[index] = newTime-time.time()
 
 print("---------------")
 print("You have " + str(len(allTimes)) + " upcoming classes" + "\n")
 
 def executeZoom():
-    global currentPeriod
-    print("Joining " + classDict['classData'][currentPeriod]["name"] + "...")
-    webbrowser.open(classDict['classData'][currentPeriod]["link"], 2)
-    currentPeriod = currentPeriod + 1
-    if(currentPeriod == 7):
-        input('Press ENTER to exit')
+    currentPeriod = min(allTimes, key=allTimes.get)
+    if(classDict['classActive'][currentPeriod]): 
+        print("Joining " + classDict['classData'][currentPeriod]["name"] + "...")
+        webbrowser.open(classDict['classData'][currentPeriod]["link"], 2)
 
-for period, t in enumerate(allTimes):
+for t in allTimes.values(): 
     timer = Timer(t, executeZoom)
     timer.start()
 
